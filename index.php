4264b6a214bd20a1ad95f1a1cb859592439d710c
thesharifdev <sharif.golang@gmail.com>
2024-01-04 19:05:03 +0600

    added: basic theme functionalities

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Woo Event</title>
+</head>
+<body>
+    <header>
+        <h1>Hello from header</h1>
+    </header>
+    <section class="hero">
+        <h1>Hello from hero section</h1>
+    </section>
+    <footer>
+        <h1>Hello from footer</h1>
+    </footer>
+</body>
+</html>
