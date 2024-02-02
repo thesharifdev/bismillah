1407a6436bf12b57d550ae687764ef8337cb7f65
thesharifdev <sharif.golang@gmail.com>
2024-02-02 21:00:42 +0600

    added: post pattern

---
+++ patterns/post.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Post
+ * Slug: bismillah/post
+ * Categories: post
+ * Viewport width: 1400
+ */
+?>
+
+<!-- wp:query {"queryId":9,"query":{"perPage":6,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"exclude","inherit":false}} -->
+<div class="wp-block-query"><!-- wp:post-template {"layout":{"type":"grid","columnCount":3}} -->
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"30px","right":"30px","bottom":"30px","left":"30px"}}},"layout":{"inherit":false}} -->
+<div class="wp-block-group" style="padding-top:30px;padding-right:30px;padding-bottom:30px;padding-left:30px"><!-- wp:post-title {"isLink":true} /-->
+
+<!-- wp:post-excerpt {"moreText":"Read more"} /-->
+
+<!-- wp:post-date /--></div>
+<!-- /wp:group -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query -->
